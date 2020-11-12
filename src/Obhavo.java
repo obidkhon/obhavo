adae34e9e5ba9762fb5e7d675f35d52eebda1a29
obidkon <obid.sharipov.80@mail.ru>
2020-11-12 21:20:59 +0500

    34

---
+++ src/Obhavo.java
@@ -0,0 +1,13 @@
+public class Obhavo {
+    public static void main(String[] args){
+
+        int harorat=32;
+        if(harorat>28){
+            System.out.println("udaqa qoling");
+
+        }
+            else {
+            System.out.println("hamma ishka");
+        }
+    }
+}
